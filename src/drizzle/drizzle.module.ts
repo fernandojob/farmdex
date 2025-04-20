637006ec48d478ce4db0348eca85925b170b5040
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-20 17:35:57 -0300

    feat: crar drizzle.module.ts

---
+++ src/drizzle/drizzle.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { db } from '../../drizzle/config';
+
+@Module({
+  providers: [
+    {
+      provide: 'DRIZZLE',
+      useValue: db,
+    },
+  ],
+  exports: ['DRIZZLE'],
+})
+export class DrizzleModule {}
