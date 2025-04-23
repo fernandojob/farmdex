9c612a152720ddd630884c3cab3ba4c176aac23d
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-22 23:46:12 -0300

    feat:Useguards jwt proteção rotas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from './modules/auth/decorators/current-user.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  getProfile(@CurrentUser() user: any) {
+    return {
+      message: 'Você acessou uma rota protegida!',
+      user,
+    };
   }
 }
