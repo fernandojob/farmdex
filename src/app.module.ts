c84526b208b8e324da3048aed5143c3b9aebf11e
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-24 00:23:12 -0300

    feat: importar FarmsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { DrizzleModule } from './database/drizzle.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ClientsModule } from './modules/clients/clients.module';
+import { FarmsModule } from './modules/farms/farms.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DrizzleModule, AuthModule, UsersModule, ClientsModule],
+  imports: [
+    DrizzleModule,
+    AuthModule,
+    UsersModule,
+    ClientsModule,
+    FarmsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
