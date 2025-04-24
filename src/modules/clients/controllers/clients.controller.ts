55c7fb92c9c2fe939083f8d8385c94abfbde757a
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-24 00:33:37 -0300

    feat: tipagem usuario autenticado

--- src/modules/clients/controllers/clients.controller.ts
+++ src/modules/clients/controllers/clients.controller.ts
@@ -3,6 +3,7 @@ import { ClientsService } from '../services/clients.service';
 import { CreateClientDto } from '../dtos/create-client.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/modules/auth/decorators/current-user.decorator';
+import { AuthenticatedUser } from '../../auth/types/authenticated-user';
 
 @Controller('clients')
 @UseGuards(AuthGuard('jwt'))
@@ -10,12 +11,12 @@ export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
-  create(@Body() dto: CreateClientDto, @CurrentUser() user: any) {
+  create(@Body() dto: CreateClientDto, @CurrentUser() user: AuthenticatedUser) {
     return this.clientsService.create(dto, user.id);
   }
 
   @Get('me')
-  getClient(@CurrentUser() user: any) {
+  getClient(@CurrentUser() user: AuthenticatedUser) {
     return this.clientsService.findByUserId(user.id);
   }
 }
