0ea5c2e1fc8d5f03c5808ba4eb8b3fb622bdf009
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-24 00:26:50 -0300

    feat: adiciona tipagem ao decorator CurrentUser

--- src/modules/auth/decorators/current-user.decorator.ts
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -1,6 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthenticatedUser } from '../types/authenticated-user';
+import { RequestWithUser } from '../types/request-with-user';
 
-export const CurrentUser = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
+export const CurrentUser = createParamDecorator((_data: unknown, ctx: ExecutionContext): AuthenticatedUser => {
+  const request = ctx.switchToHttp().getRequest<RequestWithUser>();
   return request.user;
 });
