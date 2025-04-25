fe572c7b92d1df11eb3af78b4460707e6905c9d0
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-25 13:19:34 -0300

    docs: aplicando decorator AuthRegister e AuthLogin

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { RegisterAuthDto } from '../dtos/register-auth.dto';
 import { LoginAuthDto } from '../dtos/login-auth.dto';
+import { DocAuthLogin, DocAuthRegister } from '../../../docs/decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @DocAuthRegister()
   register(@Body() dto: RegisterAuthDto) {
     return this.authService.register(dto);
   }
 
   @Post('login')
+  @DocAuthLogin()
   login(@Body() dto: LoginAuthDto) {
     return this.authService.login(dto);
   }
