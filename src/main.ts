3281c271feacd21a0ad701acce09da219c59967b
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-25 13:16:33 -0300

    feat: create document swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +19,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Farmdex API')
+    .setDescription('API para gerenciamento de clientes e fazendas')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3001);
 }
