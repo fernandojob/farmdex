da06c3af41be457f2e8e84dab3fac8c7bb884832
Fernando Jorge Nunes <fernandonunesjob@gmail.com>
2025-04-25 13:24:04 -0300

    feat: implementando repository pattern

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ClientsController } from './controllers/clients.controller';
 import { ClientsService } from './services/clients.service';
+import { ClientsRepository } from './repositories/clients.repository';
+import { DrizzleClientsRepository } from './repositories/drizzle-clients.repository';
 
 @Module({
   controllers: [ClientsController],
-  providers: [ClientsService],
+  providers: [
+    ClientsService,
+    {
+      provide: ClientsRepository,
+      useClass: DrizzleClientsRepository,
+    },
+  ],
+  exports: [ClientsService],
 })
 export class ClientsModule {}
